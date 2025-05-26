30b4da79d8483d7d31d3ba8538836c953ceeeab5
FirminoEduardo <dudu.lf@outlook.com>
2025-05-26 20:14:10 -0300

    Configuração do banco com Sequelize

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config(); // Carregar variáveis de ambiente do arquivo .env
+
+// Conexão com o banco de dados PostgreSQL
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_NAME,
+        dialect: 'postgres', // O banco de dados que estamos usando
+        port: process.env.DB_PORT,
+    }
+);
+
+module.exports = sequelize;
